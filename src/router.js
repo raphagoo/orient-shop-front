cebeede1d1e27131394aa27a51867eb69e452aa7
raphagoo <raphael.agostini@ynov.com>
2020-11-25 09:49:19 +0100

    add footer and header templates

--- src/router.js
+++ src/router.js
@@ -24,7 +24,7 @@ export const router = new Router({
 router.beforeEach((to, from, next) => {
 
     // redirect to login page if not logged in and trying to access a restricted page
-    const publicPages = ['/login', '/register'];
+    const publicPages = ['/login', '/register', '/'];
     const authRequired = !publicPages.includes(to.path);
     const loggedIn = sessionStorage.getItem('token');
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,20 @@
 <template>
-  <div class="home">
-    <p>Hello word</p>
-  </div>
+    <div class="container">
+        <Header></Header>
+        <p>Hello word</p>
+        <Footer></Footer>
+    </div>
 </template>
 
 <script>
 
+import Header from "../includes/Header.vue";
+import Footer from "../includes/Footer.vue";
 export default {
     name: 'home',
     components: {
+        Footer,
+        Header
     }
 }
 </script>
